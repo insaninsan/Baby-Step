aa8ff9f9f6f79fb1696afb5823fe61795dc29830
Şerif Bayram <86827160+insaninsan@users.noreply.github.com>
2023-12-02 11:11:07 +0300

    Add files via upload
    data types on JAVA

--- Main.java
+++ Main.java
@@ -1,23 +1,24 @@
 package org.example;
 
-// this code aim is to help you:)
-// A perfect number is the sum of its exact divisors.
+// this code aim is to show general data types in java.
 public class Main {
     public static void main(String[] args) {
+        String a = "hello world";
+        char b = 'A'; //all unicode characters
+        byte c = -127; //date types is integer
+        short d = 32000;//data types is integer
+        int e = -219090000;//data types is integer
+        long f = -128734612;//data types is integer
+        float i = -1283714f;//floating-point number
+        double j = -1.3247129812730123809124902812034;//floating-point number
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(i);
+        System.out.println(j);
 
-        int number = 6;
-        int total = 0;
-        for (int i = 1; i < number; i++) {
-            if (number % i == 0) {
-                total = total + i;
-
-            }
-            
-        }
-        if (total == number) {
-            System.out.println(number + " is a perfect number");
-        } else {
-            System.out.println(number + " is not a perfect number");
-        }
     }
 }
